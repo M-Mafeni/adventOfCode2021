5c5f597cdbaaf9b64a75761219150f0b95898ae4
M-Mafeni <mafeni99@gmail.com>
2021-12-10 22:52:46 +0000

    Add Solution for Day 7 pt 2

--- 07/Solution.scala
+++ 07/Solution.scala
@@ -5,6 +5,15 @@ type CrabPositionMap = Map[Int, Long]
 
 def calculateCostMap(crabPositionMap: CrabPositionMap, position: Int) = crabPositionMap.map((currPosition, count) => count * (currPosition - position).abs).sum
 
+def calculateCostMap2(crabPositionMap: CrabPositionMap, position: Int) = {
+    def calcBurnRate(currPosition: Int): Long = {
+        val n = (currPosition - position).abs
+        // calc sum of numbers frpm 1 to n
+        n * (n + 1) / 2
+    }
+    crabPositionMap.map((currPosition, count) => count * calcBurnRate(currPosition)).sum
+}
+
 def toCrabMap(crabPositions: List[Int]): CrabPositionMap = {
     val positionsActive = crabPositions.toSet
     val keys = List.range(positionsActive.min, positionsActive.max + 1)
@@ -13,7 +22,10 @@ def toCrabMap(crabPositions: List[Int]): CrabPositionMap = {
 
 def calculateLeastCost(crabPositionMap: CrabPositionMap): Long = crabPositionMap.keys.map(position => calculateCostMap(crabPositionMap, position)).min 
 
+def calculateLeastCost2(crabPositionMap: CrabPositionMap): Long = crabPositionMap.keys.map(position => calculateCostMap2(crabPositionMap, position)).min 
+
 @main def main = {
-    val crabPositions: CrabPostions = Source.fromFile("input.txt").mkString.split(",").map(_.toInt).toList
-    println(calculateLeastCost(toCrabMap(crabPositions)))
+    val crabMap = toCrabMap(Source.fromFile("input.txt").mkString.split(",").map(_.toInt).toList)
+    println(calculateLeastCost(crabMap))
+    println(calculateLeastCost2(crabMap))
 }
