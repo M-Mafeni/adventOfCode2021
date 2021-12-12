d2759d19daa56454f329acd00c97133910867faf
M-Mafeni <mafeni99@gmail.com>
2021-12-12 22:59:22 +0000

    Add Solution for Day 11 pt 1

---
+++ 11/Solution.scala
@@ -0,0 +1,61 @@
+import scala.io.Source
+type Octopus = Int
+type Grid = Vector[Vector[Octopus]]
+
+val example = Vector(Vector(1, 1, 1, 1, 1), Vector(1, 9, 9, 9, 1), Vector(1, 9, 1, 9, 1), Vector(1, 9, 9, 9, 1), Vector(1, 1, 1, 1, 1))
+
+def hasEnergyLevelGreaterThanNine(grid: Grid): Boolean = grid.exists(row => row.exists(_ > 9))
+def inRange(x: Int, minVal: Int, maxVal: Int): Boolean = minVal <= x && x < maxVal
+def getNeighbours(row: Int, col: Int, grid: Grid): List[Octopus] = (for {
+        i <- (row - 1) until (row + 2)
+        j <- (col - 1) until (col + 2)
+        if (inRange(i, 0, grid.length) && inRange(j, 0, grid.head.length) && (i,j) != (row, col))
+    } yield grid(i)(j)).toList
+
+def runFlashes(grid: Grid): Grid = {
+    def updateLevel(row: Int, col: Int) = {
+        val level = grid(row)(col)
+        val neighbours = getNeighbours(row, col, grid)
+        val increment = neighbours.count(_ > 9)
+        if (level == 0 || level > 9) {
+            0
+        } else {
+            level + increment
+        }
+    }
+    if (!hasEnergyLevelGreaterThanNine(grid)) {
+        grid
+    } else {
+        val rowIndexes = List.range(0, grid.length).toVector
+        val newGrid = rowIndexes.map(row => List.range(0, grid.head.length).toVector.map(col => updateLevel(row, col)))
+        runFlashes(newGrid)
+    }
+}
+// Given the grid return the no of flashes that occured + a new grid
+def runStep(grid: Grid): (Int, Grid) = {
+    // increase every energy level by one
+    val increasedByOne = grid.map(row => row.map(oct => oct + 1))
+    val afterFlashes = runFlashes(increasedByOne)
+    val flashNo = grid.map((row) => row.count(_ == 0)).sum
+    (flashNo, afterFlashes)
+}
+
+def runStepsHelper(steps: Int, grid: Grid, flashTotal: Int): Int = {
+    val (flashes, nextGrid) = runStep(grid)
+    if (steps == 0) {
+        flashTotal + flashes
+    } else {
+        runStepsHelper(steps - 1, nextGrid, flashTotal + flashes)
+    }
+}
+// run steps and return the total no of flashes
+def runSteps(steps: Int, grid: Grid): Int = runStepsHelper(steps, grid, 0)
+
+def toGrid(lines: List[String]): Grid = lines.map(line => line.toVector.map(x => (x - '0').toInt)).toVector
+
+def prettyGrid(grid: Grid): String = "\n" + grid.map(row => row.map(value => ('0' + value).toChar).mkString).mkString("\n")
+
+@main def main = {
+    val grid = toGrid(Source.fromFile("input.txt").getLines.toList)
+    println(runSteps(100, grid))
+}
