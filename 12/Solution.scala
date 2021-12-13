683f2acfc1040e43909561fcfcd46320e1acb930
M-Mafeni <mafeni99@gmail.com>
2021-12-13 00:16:43 +0000

    Add Solution for Day 12 pt 2

--- 12/Solution.scala
+++ 12/Solution.scala
@@ -30,8 +30,50 @@ def getPaths(graph: Graph): Set[Path] = {
     getPathHelper("start", Set.empty, List())
 }
 
+
+def getPaths2(graph: Graph): Set[Path] = {
+    // Update vistied and check if path is still valid
+    def updateVisited(visited: Map[Cave, Int], cave: Cave): Option[Map[Cave, Int]] = {
+        if (!isSmallCave(cave)) {
+            Some(visited)
+        } else {
+            /* If a cave has already been visited twice (2xCave)
+                If (cave == 2xCave || cave has already been visited) return failure string // can't visit cave any more times
+                else add to Map
+               else Add to Map
+            */
+            val optionalTwiceCave = visited.find((_, count) => count == 2).map(_._1)
+            optionalTwiceCave match {
+               case None => Some(visited.updated(cave, visited.getOrElse(cave, 0) + 1))
+               case Some(twiceCave) => if (cave == twiceCave || visited.get(cave).isDefined) None else Some(visited.updated(cave, visited.getOrElse(cave, 0) + 1))
+            }
+        }
+    }
+    def getPathHelper(source: Cave, visited: Map[Cave, Int], currPath: Path): Set[Path] = {
+        if (source == "start" && !currPath.isEmpty) {
+            // Invalid Path
+            Set.empty
+        } else if (source == "end") {
+            Set((source :: currPath).reverse)
+        } else {
+            val neighbours = graph.getOrElse(source, Set.empty)
+            val optionalUpdatedVisited = updateVisited(visited, source)
+            optionalUpdatedVisited match {
+                case None => Set.empty
+                case Some (newVisited) => {
+                    val optionalPaths = neighbours.map(cave => getPathHelper(cave, newVisited, source :: currPath)).reduceLeftOption(_ | _)
+                    if optionalPaths.isEmpty then Set.empty else optionalPaths.get
+                }
+            }
+
+        }
+    }
+    getPathHelper("start", Map.empty, List())
+}
+
 @main def main = {
     val lines = Source.fromFile("input.txt").getLines.toList
     val graph = toGraph(lines)
     println(getPaths(graph).size)
+    println(getPaths2(graph).size)
 }
